e89922e7cc4ed1b7dccdd8f343e290a615caf508
Javier Estevez <javierdaniloestevez@gmail.com>
2022-05-10 04:55:00 -0300

    agrego componente about con sucursales de la tienda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { LanzamientosComponent } from './pages/lanzamientos/lanzamientos.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LanzamientosComponent
+    LanzamientosComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
