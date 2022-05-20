8b5f9b9c25fa26ac417d95794af6f65c3e0dad18
Javier Estevez <javierdaniloestevez@gmail.com>
2022-05-20 05:33:54 -0300

    implemento ver el siguiente articulo desde la pagina del producto

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -11,6 +11,7 @@ import { ProductoDescripcion } from '../../interfaces/producto-descripcion.inter
 export class ItemComponent implements OnInit {
 
   id: string = '';
+  nextId: string = '';
   producto!: ProductoDescripcion;
 
   constructor( private activatedRoute: ActivatedRoute, 
@@ -19,17 +20,34 @@ export class ItemComponent implements OnInit {
   ngOnInit(): void {
 
     this.activatedRoute.params.subscribe( params => {
-      console.log(params['id']);
+      //console.log(params['id']);
       this.id = params['id'];            
       this.productosService.getProducto(params['id']).subscribe( (res: any) => {
         //console.log(res);
         this.producto = res;
-        console.log(this.producto);
+        //console.log(this.producto);
         
-        this.productosService.loading = false;        
+        this.productosService.loading = false;
+        this.nextId = this.siguienteArticulo(this.id);        
       });       
     })
 
+  }
+
+  siguienteArticulo(codigo: string): string {
+
+    let arr = codigo.split('-');
+    let siguiente = Number.parseInt(arr[1]) + 1;
+
+    // controlar que si es el 15 vuelvo al 1
+    if (siguiente > 15)
+      siguiente = 1;
+
+    arr[1] = siguiente.toString();
+    
+    let resultado = arr.join('-');
+    
+    return resultado;
   }
 
 }
